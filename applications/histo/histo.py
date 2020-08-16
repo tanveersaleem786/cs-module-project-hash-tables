c9935d866d8e1be5ba96fe19c45244636bb4c26c
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-08-16 17:24:46 -0400

    Day 4: Start Working

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,9 @@
 # Your code here
+with open('robin.txt') as f:
+    # read_data = f.read()
+    # print(read_data)
+    for line in f:
+        print(line, end='')
+
+    f.close()
 
--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,6 +1,7 @@
 # Your code here
 import random
 import math
+import time
 
 lookup_table = {}
 
@@ -22,10 +23,15 @@ def slowfun(x, y):
         lookup_table[(x, y)] = slowfun_too_slow(x, y)
     return lookup_table[(x, y)]
 
+
+
+start_time = time.time()
 # Do not modify below this line!
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')    
+    print(f'{i}: {x},{y}: {slowfun(x, y)}')  
+end_time = time.time()
+print(end_time - start_time)  
 
 
